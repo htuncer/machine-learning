6ad189d758f4183e3e288ffba1e056a135f0e21f
Hasan Tuncer <hasan.tuncer.dev@gmail.com>
2017-09-29 16:00:27 -0700

    .

--- projects/capstone/create_tfrecord.py
+++ projects/capstone/create_tfrecord.py
@@ -17,41 +17,44 @@ import time
 # https://github.com/tensorflow/models/blob/master/object_detection/g3doc/using_your_own_dataset.md
 
 
-def img_to_tf_record(row, img_dir, label_map):
+def img_to_tf_record(group, img_dir, label_map):
     """Creates a tf.Example proto from sample cat image.
     Args:
     encoded_cat_image_data: The png encoded data of the cat image.
     Returns:
     example: The created tf.Example.
     """
-    img_name = row['Frame']
-    img_path = img_dir + img_name
-    img = np.array(Image.open(img_path))
+    xmins = list()
+    xmaxs = list()
+    ymins = list()
+    ymaxs = list()
+    label_text = list()
+    label = list()
 
-    height = img.shape[0]
-    width = img.shape[1]
-    image_format = b'jpg'
+    flag = False
+    for i, row in group.iterrows():
+        if not flag:
+            img_name = "%s"%row['Frame']
+            img_path = img_dir + img_name
+            img = np.array(Image.open(img_path))
+            height = img.shape[0]
+            width = img.shape[1]
+            image_format = b'jpg'
+            with tf.gfile.GFile(img_path, 'rb') as fid:
+                encoded_image_data = fid.read()
 
-    with tf.gfile.GFile(img_path, 'rb') as fid:
-        encoded_image_data = fid.read()
-
-    xmins = [row['xmin'] / float(width)]
-    xmaxs = [row['xmax'] / float(width)]
-    ymins = [row['ymin'] / float(height)]
-    ymaxs = [row['ymax'] / float(height)]
-
-    label_text = [row['Label']]
-    label = [label_map[row['Label']]]
-    # if xmins[0] > 1.01 or xmaxs[0] > 1 or ymins[0] > 1 or ymaxs[0] > 1:
-    #     print("name:%s label_text %s label %s xmin %s xmax %s ymin %s ymax %s" %
-    #           (img_name, label_text[0], label[0], xmins, xmaxs, ymins, ymaxs))
-    #     sys.exit(-1)
-
+        flag = True
+        xmins.append(row['xmin'] / float(width))
+        xmaxs.append(row['xmax'] / float(width))
+        ymins.append(row['ymin'] / float(height))
+        ymaxs.append(row['ymax'] / float(height))
+        label_text.append(str.encode(row['Label']))
+        label.append(label_map[row['Label']])
     tf_record = tf.train.Example(features=tf.train.Features(feature={
         'image/height': dataset_util.int64_feature(height),
         'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(img_name),
-        'image/source_id': dataset_util.bytes_feature(img_name),
+        'image/filename': dataset_util.bytes_feature(str.encode(img_name)),
+        'image/source_id': dataset_util.bytes_feature(str.encode(img_name)),
         'image/encoded': dataset_util.bytes_feature(encoded_image_data),
         'image/format': dataset_util.bytes_feature(image_format),
         'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
@@ -76,28 +79,29 @@ if __name__ == "__main__":
     data = pd.read_csv(label_file)
     #drop the rows for Pedestrian
     data = data[data.Label != 'Pedestrian']
+    tf_records = dict()
+    
+    for name, group in data.groupby('Frame'):
+        tf_records[name]=img_to_tf_record(group, img_dir, label_map)
+    keys = list(tf_records.keys())
+    print(keys)
+    random.shuffle(keys)
+    training_size = int(len(keys)*0.8)
 
-    # in-place shuffeling
-    data = data.sample(frac=1).reset_index(drop=True)
-
-    #training_size = int(data.shape[0] * 0.7)
-    training_size=10
-    validation_size = int(training_size * 0.25)
 
     print('Creating train.record - size:%d' % training_size)
     output_path = PROJECT_PATH + "/data/train.record"
     writer = tf.python_io.TFRecordWriter(output_path)
-    for index, row in data[:training_size].iterrows():
-        tf_record = img_to_tf_record(row, img_dir, label_map)
-        writer.write(tf_record.SerializeToString())
+    for key in keys[0:training_size]:
+        writer.write(tf_records[key].SerializeToString())
     writer.close()
     print ('completed train.record\n\n\n')
+    validation_size = len(keys) - training_size
     print ('creating val.record - size:%d' % validation_size)
     output_path = PROJECT_PATH + "/data/val.record"
     writer = tf.python_io.TFRecordWriter(output_path)
-    for index, row in data[data.shape[0]-validation_size:].iterrows():
-        tf_record = img_to_tf_record(row, img_dir, label_map)
-        writer.write(tf_record.SerializeToString())
+    for key in keys[training_size:]:
+        writer.write(tf_records[key].SerializeToString())
     writer.close()
     print ('completed val.record\n\n\n')
     end = time.time()
